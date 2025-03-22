67c77fc3be90cc556490d82de16992d609a560e4
Brendon <vineyabn207@potsdam.edu>
2025-03-21 22:49:50 -0400

    added upgrade class

---
+++ core/src/main/java/cellcorp/gameofcells/objects/MitochondriaUpgrade.java
@@ -0,0 +1,10 @@
+package cellcorp.gameofcells.objects;
+
+import cellcorp.gameofcells.screens.ShopScreen;
+
+/**
+ * Mitochondria upgrade.
+ */
+public class MitochondriaUpgrade extends Upgrade {
+    
+}
--- core/src/main/java/cellcorp/gameofcells/objects/Upgrade.java
+++ core/src/main/java/cellcorp/gameofcells/objects/Upgrade.java
@@ -23,4 +23,37 @@ public class Upgrade {
                 this.requiredATP = requiredATP;
                 this.requiredSize = requiredSize;
     }
+
+    /**
+     * Check if the player can purchase this upgrade
+     */
+    public boolean canPurchase(Cell cell, ShopScreen shopScreen) {
+        if (cell.getCellATP() < cost || cell.getCellDiameter() < requiredSize) {
+            return false;
+        }
+
+        // Check if previous upgrade is purchased
+        return isPreviousUpgradePurchased(shopScreen);
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRequiredATP() {
+        return requiredATP;
+    }
+
+    public int getRequiredSize() {
+        return requiredSize;
+    }
 }
