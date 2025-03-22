3c332c7113457632f1d3af026600c69f00581c59
Brendon <vineyabn207@potsdam.edu>
2025-03-21 22:56:45 -0400

    implemented mitochondria object for upgrade

--- core/src/main/java/cellcorp/gameofcells/objects/MitochondriaUpgrade.java
+++ core/src/main/java/cellcorp/gameofcells/objects/MitochondriaUpgrade.java
@@ -6,5 +6,18 @@ import cellcorp.gameofcells.screens.ShopScreen;
  * Mitochondria upgrade.
  */
 public class MitochondriaUpgrade extends Upgrade {
-    
+    public MitochondriaUpgrade() {
+        super("Mitochondria", 30, "Increases ATP production", 0, 0);
+    }
+
+    @Override
+    public void applyUpgrade(Cell cell) {
+        cell.setHasMitochondria(true);
+    }
+
+    @Override
+    public boolean isPreviousUpgradePurchased(ShopScreen shopScreen) {
+        // Mitochondria is the first upgrade, so no previous upgrade it required.
+        return true;
+    }
 }
