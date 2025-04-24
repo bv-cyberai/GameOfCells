9518103a8c4c7e8ad7bf430b01836454348714cc
Brendon Vineyard <vineyabn207@potsdam.edu>
2025-04-24 19:11:18 -0400

    Fix SPACE/ENTER key causing instant menu selection after attract screen

--- core/src/main/java/cellcorp/gameofcells/providers/DefaultInputProvider.java
+++ core/src/main/java/cellcorp/gameofcells/providers/DefaultInputProvider.java
@@ -1,31 +1,13 @@
 package cellcorp.gameofcells.providers;
 
 import com.badlogic.gdx.Gdx;
-import java.util.HashMap;
-import java.util.Map;
 
 /// Implementation of [InputProvider] which calls `Gdx.input` for everything.
 public class DefaultInputProvider implements InputProvider {
 
-    private final Map<Integer, Boolean> previousKeyStates = new HashMap<>();
-
     @Override
     public boolean isKeyJustPressed(int key) {
-        boolean isPressed = Gdx.input.isKeyPressed(key);
-        boolean wasPressed = previousKeyStates.getOrDefault(key, false);
-
-        // Update state immediately
-        previousKeyStates.put(key, isPressed);
-
-        // Return true only if the key was JUST pressed
-        boolean result = isPressed && !wasPressed;
-
-        // If this was a "just pressed" event, mark it as "already pressed" for next frame
-        if (result) {
-            previousKeyStates.put(key, true);
-        }
-
-        return result;
+        return Gdx.input.isKeyJustPressed(key);
     }
 
     @Override
